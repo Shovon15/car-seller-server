7853730ea518a52dff18d139b8ad86a1285c28b1
Shovon15 <shovonrnbd@gmail.com>
2022-11-24 01:24:59 +0600

    mongodb setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const jwt = require("jsonwebtoken");
+require("dotenv").config();
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jallqro.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+// console.log(uri);
+// ------------------------
+app.get("/", async (req, res) => {
+    res.send("car resale server is running");
+});
+
+app.listen(port, () => console.log(`Car resale server running on ${port}`));
