8aefa56c567b3ae9719e8db5c8fac4ab8e242e3b
Shovon15 <shovonrnbd@gmail.com>
2022-11-26 21:59:35 +0600

    add booking api

--- index.js
+++ index.js
@@ -26,11 +26,18 @@ async function run() {
         const usersCollection = client.db("carResaleMarket").collection("users");
         const categoryCollection = client.db("carResaleMarket").collection("carCategories");
         const productsCollection = client.db("carResaleMarket").collection("products");
+        const bookingsCollection = client.db("carResaleMarket").collection("bookings");
         // ---------------------products api-----------------------------
 
         app.get("/carCategory", async (req, res) => {
             const filter = {};
             const result = await categoryCollection.find(filter).toArray();
+            res.send(result);
+        });
+
+        app.post("/products", async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
             res.send(result);
         });
 
@@ -46,21 +53,65 @@ async function run() {
         app.get("/products/:category/:_id", async (req, res) => {
             const category = req.params.category;
             const id = req.params._id;
-            console.log(category, id);
+            // console.log(category, id);
             const filter = { _id: ObjectId(id) };
             const result = await productsCollection.findOne(filter);
+            res.send(result);
+        });
+
+        // ------------------------bookings api-------------------
+        app.post("/bookings", async (req, res) => {
+            const booking = req.body;
+            // console.log(user);
+            // TODO: make sure you do not enter duplicate user email
+            // only insert users if the user doesn't exist in the database
+            const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         });
 
         // ---------------------users api-----------------------------
         app.post("/users", async (req, res) => {
             const user = req.body;
-            console.log(user);
+            // console.log(user);
             // TODO: make sure you do not enter duplicate user email
             // only insert users if the user doesn't exist in the database
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+        app.get("/users/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send(user);
+        });
+
+        // --------------------------admin access api--------------------------
+
+        app.get("/buyers", async (req, res) => {
+            const query = { userRole: "buyer" };
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+        app.get("/sellers", async (req, res) => {
+            const query = { userRole: "seller" };
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+        app.put("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    verify: "true",
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+        // -----------------------------------------
     } finally {
     }
 }
